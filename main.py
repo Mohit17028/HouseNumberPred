88fbeaf76b48da14c1aff60de8a5f97d410a7e5b
Mohit17028 <mohit17028@iiitd.ac.in>
2020-12-02 01:55:17 +0530

    making a UI interface

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 import torch
+import os
+import flask
 from torch_utils import transform_image, get_prediction
 
 app = Flask(__name__)
@@ -14,13 +16,15 @@ def allowed_file(filename):
 
 @app.route('/')
 def home():
-    print('success')
+    data = {}
+    return flask.render_template("index.html", data=data)
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        file = request.files.get('file')
+        print(request.files)
+        file = request.files.read()
 
         if file is None or file.filename == "":
             return jsonify({'error': 'no file'})
@@ -29,17 +33,62 @@ def predict():
 
         # try:
         # print(file.filename)
+        print("reachd")
         img_bytes = file.read()
         tensor = transform_image(img_bytes)
         length_digit, output_number = get_prediction(tensor)
         print(length_digit, output_number)
         data = {'prediction': output_number, 'Length': str(length_digit)}
+        return flask.render_template("index.html", data=data)
 
-        return jsonify(data)
         # except:
         #     return jsonify({'error': 'error during prediction'})
 
+@app.route('/api/classify', methods=['POST', 'GET'])
+def upload_file():
+    if flask.request.method == 'GET':
+        url = flask.request.args.get("url")
+        img = load_image_url(url)
+        tensor = transform_image(img)
+        length_digit, output_number = get_prediction(tensor)
+
+    else:
+        img_bytes = flask.request.files['file'].read()
+        tensor = transform_image(img_bytes)
+        length_digit, output_number = get_prediction(tensor)
+        # print(length_digit, output_number)
+
+    data = {'prediction': output_number, 'Length': str(length_digit)}
+    return flask.jsonify(data)
+
+
+@app.after_request
+def add_header(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+
+    response.cache_control.max_age = 0
+    return response
+
+
+@app.route('/<path:path>')
+def static_file(path):
+    if ".js" in path or ".css" in path:
+        return app.send_static_file(path)
+    else:
+        return app.send_static_file('index.html')
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
+def before_request():
+    app.jinja_env.cache = {}
 
 if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
     app.debug = True
     app.run()
